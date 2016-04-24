dd769f3550071238f92f733fbef069874f088a91
Jacques Uys <jacquesuys@Jacquess-MacBook-Air.local>
2016-04-24 15:11:11 +0200

    Tabs fix

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
