ddf1f4e96ab7529d6d5c880b60ecfeff046ea556
Jacques Uys <jacquesuys@Jacquess-MacBook-Air.local>
2016-04-24 15:09:44 +0200

    'Hello World' on GET

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const hostname = '127.0.0.1';
 const port = 3000;
 
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
 app.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
